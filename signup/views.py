b536d02e78e52706778a0647a03e267a11978026
Ryan Forbes <ryfo18@gmail.com>
2016-02-24 15:38:35 -0600

    Signup working

--- signup/views.py
+++ signup/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, permissions, mixins, generics
 from signup.serializers import SignupSerializer
 from django.contrib.auth import get_user_model
+from signup.models import UserAuthenticate
 
 import random
 import string
@@ -16,15 +17,15 @@ class SignupViewSet(mixins.CreateModelMixin,
 
   def perform_create(self, request, *args, **kwargs):
     serializer = SignupSerializer(data=request.data)
-    print(serializer)
     if serializer.is_valid(): #TODO add error handling
       temp_pw = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(32))
       u = get_user_model().objects.create(email=serializer['username'], password=temp_pw, is_active=False)
       u.save()
 
-      user = get_user_model().objects.filter(email=serializer['username'])
-      print(user)
-      auth_path = "{%s_%s}".format(str(user.id, ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))))
-      auth = UserAuthenticate(user.id, auth_path)
+      user = get_user_model().objects.get(email=serializer['username'])
+      print(user.id)
+      auth_path = "{:s}_{:s}".format(str(user.id), ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10)))
+      print(auth_path)
+      auth = UserAuthenticate(user=user, auth_path=auth_path)
       auth.save()
       #TODO send e-mail w/ link
