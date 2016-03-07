f40b450e686be4faefc04305ab408df11c18e4db
Ryan Forbes <ryfo18@gmail.com>
2016-03-07 16:29:48 -0600

    Validation working

--- signup/urls.py
+++ signup/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url, include
 from rest_framework import renderers
 from rest_framework.routers import DefaultRouter
-from signup.views import UserCreate, ValidateSignupDetail
+from signup.views import UserCreate
 
 router = DefaultRouter()
 #router.register(r'create', SignupViewSet, base_name='signup')
 
 urlpatterns = [
-  url(r'^\?auth_path=\w+/$', ValidateSignupDetail.as_view()),
   url(r'^', UserCreate.as_view()),
 ]
--- signup/views.py
+++ signup/views.py
@@ -4,33 +4,16 @@ from signup.models import UserAuthenticate
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.views import APIView
 from rest_framework_jwt.settings import api_settings
 
 # Create your views here.
-class UserCreate(CreateAPIView):
+class UserCreate(APIView):
   """
   API endpoint that allows users to be added
   """
   permission_classes = (permissions.AllowAny,)
   throttle_scope = 'signup'
-
-  # POST handler
-  def post(self, request):
-    serializer = SignupSerializer(data=request.data)
-    if serializer.is_valid(): #TODO add error handling
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class ValidateSignupDetail(APIView):
-  """
-  API endpoint for signup confirmation.
-  """
-  permission_classes = (permissions.AllowAny,)
-  # TODO Need to have a throttle on this for sure
-#  throttle_scope = 'validate-signup'
 
   def get_object(self, auth_path):
     try:
@@ -40,9 +23,18 @@ class ValidateSignupDetail(APIView):
 
   def get(self, request, format=None):
     auth_path = self.request.query_params.get('auth_path', None)
-    print(auth_path)
     user = self.get_object(auth_path)
-    print(user.user)
+    user.user.is_active = True
+    user.user.save()
     serializer = SignupSerializer(user.user)
     print(serializer.data)
+    user.delete()
     return Response(serializer.data)
+
+  # POST handler
+  def post(self, request):
+    serializer = SignupSerializer(data=request.data)
+    if serializer.is_valid(): #TODO add error handling
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
